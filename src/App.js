9e03f7d39d2176bb1d2e42ff534d124a7eff92ea
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-03-03 01:33:26 -0330

    Make a few changes

--- src/App.js
+++ src/App.js
@@ -15,92 +15,173 @@ function App() {
       <section className="SocialLinks">
         <ul>
           <li><a href="https://github.com/hamzahap"><img src="./github.png" alt="GitHub " />Github</a></li>
-          <li><a href="https://www.linkedin.com/in/hamzahpunjabi/"><img src="./linkedin.png" alt="LinkedIn" />LinkedIn</a></li>
-          <li><a href="./HamzahPunjabi.pdf" target="_blank">Resume</a> </li>
+          <li><a href="https://www.linkedin.com/in/hamzahpunjabi/"><img src="./linkedin.png" alt="LinkedIn" /></a> </li>
+          <li><a href="./HamzahPunjabi.pdf" target="_blank" rel="noopener noreferrer">Resume</a> </li>
           <li><a href="mailto:hamzahqatar123@gmail.com">Email</a> </li>
-          <li>St. John's, NL, Canada</li>
+          <li><a href="https://www.google.com/maps/place/St.+John's,+NL/@47.4825459,-52.9697214,11z/data=!3m1!4b1!4m6!3m5!1s0x4b0ca38e6b0aa261:0x9e1fd4001f12261f!8m2!3d47.5556097!4d-52.7452511!16zL20vMGo4cDY">St. John's, NL, Canada</a></li>
         </ul>
       </section>
 
       {/* Education */}
-      <section>
-      <h2>Education</h2>
-      <ul>
-        <li>
-          <strong>Bachelor of Science – Computer Science, MUN</strong><br />
-          January 2019 – August 2023<br />
-          CS Average: 90%
-        </li>
-        <li>
-          <strong>Diploma in Information Systems Software (58 credits), CNA-Q</strong><br />
-          September 2017 – December 2018<br />
-          GPA: 4.0
+      <section className="EducationSection">
+        <h2>Education</h2>
+        <ul>
+          <li>
+            <strong>Bachelor of Science – Computer Science, MUN</strong><br />
+            January 2019 – August 2023<br />
+            GPA: 3.4
+          </li>
+          <li>
+            <strong>Diploma in Information Systems Software (58 credits), CNA-Q</strong><br />
+            September 2017 – December 2018<br />
+            GPA: 4.0
           </li>
         </ul>
       </section>
 
       {/* Work Experience */}
-      <section>
-      <h2>Work Experience</h2>
-      <ul>
-        <li>
-          <strong>Software Developer Intern, SmartICE</strong><br />
-          September 2021 - August 2022<br />
-          <ul>
-            <li>Maintained a dashboard application for SmartICE technology utilizing Laravel, ReactJS, ChartJS and Leaflet.</li>
-            <li>Integrated REST API functionality and caching into the dashboard to optimize database query performance.</li>
-            <li>Streamlined team collaboration and project management for SmartICE technology with Jira, Asana, and Github.</li>
-            <li>Managed codebase with Git for efficient version control.</li>
-            <li>Wrote Python encrypted database backup script to safeguard server data.</li>
-            <li>Programmed ADXL343 accelerometer into the SmartBUOY board in C using CCS compiler as a feature.</li>
-            <li>Developed a Windows Forms feature in C# for SmartQAMUTIK data that enabled remote report generation, PDF conversion, Dropbox saving via Dropbox API, as well as remote viewing and sharing of SmartQAMUTIK data, resulting in improved efficiency and productivity.</li>
-            <li>Documented comprehensive work instructions for clear and accurate knowledge sharing.</li>
-          </ul>
-        </li>
-      </ul>
+      <section className="WorkExperienceSection">
+        <h2>Work Experience</h2>
+        <ul>
+          <li>
+            <strong>Software Developer Intern, SmartICE</strong><br /><br />
+            September 2021 - August 2022<br /><br />
+            <ul>
+              <li>Maintained a dashboard application for SmartICE technology utilizing Laravel, ReactJS, ChartJS and Leaflet.</li>
+              <li>Integrated REST API functionality and caching into the dashboard to optimize database query performance.</li>
+              <li>Streamlined team collaboration and project management for SmartICE technology with Jira, Asana, and Github.</li>
+              <li>Managed codebase with Git for efficient version control.</li>
+              <li>Wrote Python encrypted database backup script to safeguard server data.</li>
+              <li>Programmed ADXL343 accelerometer into the SmartBUOY board in C using CCS compiler as a feature.</li>
+              <li>Developed a Windows Forms feature in C# for SmartQAMUTIK data that enabled remote report generation, PDF conversion, Dropbox saving via Dropbox API, as well as remote viewing and sharing of SmartQAMUTIK data, resulting in improved efficiency and productivity.</li>
+              <li>Documented comprehensive work instructions for clear and accurate knowledge sharing.</li>
+            </ul>
+          </li>
+        </ul>
       </section>
 
-
-      {/* Projects */}
-      <section>
-      <h2>Projects</h2>
-      <ul>
-        <li>Wine Quality (Fall 2023) - Conducted a data preprocessing project in Jupyter Notebook analyzing the Wine Quality Data Set from the UCI Machine Learning Repository. Conducted supervised learning, which included data analysis, feature engineering, creating missing values, and prediction of those missing values.</li>
-        <li>EncryptionDecryption (Winter 2022) - Created a simple Encryption/Decryption/Backup script using PyMySQL, pandas, and Cryptography to back up database tables with CSV files.</li>
-        <li>Blood2D (Fall 2022) - Developed a game using the SFML library in C++ as a group project. The game included advanced features such as collisions, inventory, game progression, shaders, custom user interface, advanced NPCs, and a custom level creation tool.</li>
-        <li>Canadian Pollution Visualizer (Winter 2021) - Developed a web program using NodeJS for the backend and ReactJS for the frontend as a group project. The program enabled users to generate various types of graphs and filter data for the graphs.</li>
-        <li>Quoridor Board Game (Fall 2020) - Built a board game using Java’s Swing and AWT components as a group project. The game allowed for individual player turns, movement of objects, calculating score, and a pathfinding algorithm.</li>
-        <li>Hangman (Fall 2017) - Created a hangman game using Python and the tkinter library as a course project. The game utilized a user-created word list to randomly choose words.</li>
-        <li>BlockWars (Ongoing) - Developing a 2D multiplayer game using C# and Unity. The game includes various PvP modes and uses Steam’s API for hosting and multiplayer functionality.</li>
-      </ul>
-    </section>
-
-
       {/* Certificates */}
-      <section>
+      <section className="CertificatesSection">
         <h2>Certificates</h2>
         <ul>
           {/* Add your certificates details here */}
+          Will add them soon
         </ul>
       </section>
 
-      {/* Skills */}
-      <section>
-        <h2>Skills</h2>
+      {/* Projects */}
+      <section className="ProjectsSection">
+        <h2>Projects</h2>
         <ul>
-          <li>Languages: Python, HTML, CSS, SQL, C#, C, C++, Java, Fortran, PHP, JavaScript</li>
-          <li>Frameworks: React.js, Laravel, Node.js, Docker, REST</li>
-          <li>Tools: Git, MongoDB, MySQL, SQLite, Heroku, Unity, Jupyter, Visual Studio</li>
-        </ul>
-      </section>
+          <li>
+            <strong>Wine Quality (Fall 2023)</strong><br />
+        Conducted a data preprocessing project in Jupyter Notebook analyzing the Wine Quality Data Set from the UCI Machine Learning Repository. Conducted supervised learning, which included data analysis, feature engineering, creating missing values, and prediction of those missing values.
+      </li>
+      <li>
+        <strong>EncryptionDecryption (Winter 2022)</strong><br />
+        Created a simple Encryption/Decryption/Backup script using PyMySQL, pandas, and Cryptography to back up database tables with CSV files.
+      </li>
+      <li>
+        <strong>Blood2D (Fall 2022)</strong><br />
+        Developed a game using the SFML library in C++ as a group project. The game included advanced features such as collisions, inventory, game progression, shaders, custom user interface, advanced NPCs, and a custom level creation tool.
+      </li>
+      <li>
+        <strong>Canadian Pollution Visualizer (Winter 2021)</strong><br />
+        Developed a web program using NodeJS for the backend and ReactJS for the frontend as a group project. The program enabled users to generate various types of graphs and filter data for the graphs.
+      </li>
+      <li>
+        <strong>Quoridor Board Game (Fall 2020)</strong><br />
+        Built a board game using Java’s Swing and AWT components as a group project. The game allowed for individual player turns, movement of objects, calculating score, and a pathfinding algorithm.
+      </li>
+      <li>
+        <strong>Hangman (Fall 2017)</strong><br />
+        Created a hangman game using Python and the tkinter library as a course project. The game utilized a user-created word list to randomly choose words.
+      </li>
+      <li>
+        <strong>BlockWars (Ongoing)</strong><br />
+        Developing a 2D multiplayer game using C# and Unity. The game includes various PvP modes and uses Steam’s API for hosting and multiplayer functionality.
+      </li>
+    </ul>
+  </section>
 
+  {/* Skills */}
+  <section className="SkillsSection">
+    <h2>Skills</h2>
+    <ul>
+      <li>
+        <i className="fab fa-python"></i>
+        <div>
+          <h3>Python</h3>
+          <p>Experience with data analysis, machine learning, and web development using frameworks such as Flask.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fab fa-html5"></i>
+        <div>
+          <h3>HTML/CSS</h3>
+          <p>Proficient in creating responsive and visually appealing web pages using modern web standards.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fas fa-database"></i>
+        <div>
+          <h3>Databases</h3>
+          <p>Experience with MySQL and MongoDB including database design, optimization, and maintenance.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fab fa-react"></i>
+        <div>
+          <h3>React.js</h3>
+          <p>Experience with building web applications using React.js, including state management, component composition, and testing.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fab fa-node-js"></i>
+        <div>
+          <h3>Node.js</h3>
+          <p>Experience with building server-side web applications using Node.js, including creating RESTful APIs and handling user authentication in Laravel.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fab fa-docker"></i>
+        <div>
+          <h3>Docker</h3>
+          <p>Familiarity with containerization technology and experience with deploying applications using Docker containers.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fas fa-code-branch"></i>
+        <div>
+          <h3>Git</h3>
+          <p>Experience with version control using Git and familiarity with GitHub workflow.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fas fa-database"></i>
+        <div>
+          <h3>Databases</h3>
+          <p>Experience working with various databases including MongoDB, MySQL, and SQLite. Familiarity with writing complex queries, indexing, and database design principles.</p>
+        </div>
+      </li>
+      <li>
+        <i className="fas fa-desktop"></i>
+        <div>
+          <h3>Visual Studio</h3>
+          <p>Experience with using Visual Studio for development of various applications, including C# Windows Forms and ASP.NET Web Applications.</p>
+        </div>
+      </li>
+    </ul>
+  </section>
 
-      {/* Footer */}
-      <footer>
-        <p>&copy; {new Date().getFullYear()} Hamzah Punjabi. All rights reserved.</p>
-      </footer>
-    </div>
-  );
+  {/* Footer */}
+  <footer>
+    <p>&copy; {new Date().getFullYear()} Hamzah Punjabi. All rights reserved.</p>
+  </footer>
+</div>
+);
 }
 
 export default App;
+
+
