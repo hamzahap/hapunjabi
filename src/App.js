60688f1cf9dfa8b6b5136e4cd8a62d466e234e07
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-09-10 18:29:41 -0230

    Add transcript

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import './App.css';
 import RockShooterGame from './rock';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAws, faGit, faHtml5, faPython } from '@fortawesome/free-brands-svg-icons';
-import { faCloud, faCode, faDatabase, faDesktop, faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { faAws, faGit, faGithub, faHtml5, faLinkedin, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faCloud, faCode, faDatabase, faDesktop, faEnvelope, faFile, faFileAlt, faGamepad, faLocationCrosshairs, faScroll, faUniversity } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
@@ -11,17 +11,18 @@ function App() {
       {/* Introduction */}
       <header>
         <h1>Hello, I'm Hamzah Punjabi</h1>
-        <p>I am a Computer Science student in my final year at MUN.</p>
+        <p>I am a Software Developer recently graduated from MUN.</p>
       </header>
 
       {/* Social links */}
       <section className="SocialLinks">
         <ul>
-          <li><a href="https://github.com/hamzahap" target="_blank" rel="noopener noreferrer"><img src="./github1.png" alt="GitHub" /></a></li>
-          <li><a href="https://www.linkedin.com/in/hamzahpunjabi/" target="_blank" rel="noopener noreferrer"><img src="./linkedin.png" alt="LinkedIn" /></a> </li>
-          <li><a href="./ResumeHamzahPunjabi.pdf" target="_blank" rel="noopener noreferrer"><img src="./resume.png" alt="Resume" /></a> </li>
-          <li><a href="mailto:hamzahqatar123@gmail.com"><img src="./email.png" alt="Email" /></a> </li>
-          <li><a href="https://www.google.com/maps/place/St.+John's,+NL/@47.4825459,-52.9697214,11z/data=!3m1!4b1!4m6!3m5!1s0x4b0ca38e6b0aa261:0x9e1fd4001f12261f!8m2!3d47.5556097!4d-52.7452511!16zL20vMGo4cDY" target="_blank" rel="noopener noreferrer"><img src="./location.png" alt="Location" /></a></li>
+          <li><a href="https://github.com/hamzahap" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} color='#000000' size="2x" /></a></li>
+          <li><a href="https://www.linkedin.com/in/hamzahpunjabi/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} color='#000000' size="2x" /></a> </li>
+          <li><a href="./ResumeHamzahPunjabi.pdf" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileAlt} color='#000000' size="2x" /></a> </li>
+          <li><a href="mailto:hamzahqatar123@gmail.com"><FontAwesomeIcon icon={faEnvelope} color='#000000' size="2x" /></a> </li>
+          <li><a href="https://www.google.com/maps/place/St.+John's,+NL/@47.4825459,-52.9697214,11z/data=!3m1!4b1!4m6!3m5!1s0x4b0ca38e6b0aa261:0x9e1fd4001f12261f!8m2!3d47.5556097!4d-52.7452511!16zL20vMGo4cDY" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLocationCrosshairs} color='#000000' size="2x" /></a></li>
+          <li><a href="./FinalOfficialTranscript.pdf"><FontAwesomeIcon icon={faScroll} color='#000000' size="2x" /></a> </li>
         </ul>
       </section>
 
@@ -74,7 +75,7 @@ function App() {
       <h2>Projects</h2>
       <ul>
       <li>
-        <strong>LaTeX Resume Tailor (Spring 2023)</strong><br />
+        <a href='https://github.com/hamzahap/resume_tailor' target="_blank" rel="noopener noreferrer"><strong>LaTeX Resume Tailor (Spring 2023)</strong></a><br />
         <p>Developed a local web application using Python (Flask) for the backend and ReactJS for the frontend. The tool allows users to upload a LaTeX resume, select, rearrange, and edit specific sections, and then generate a tailored version emphasizing relevant experiences and skills for job applications.</p>
       </li>
       <li>
@@ -82,7 +83,7 @@ function App() {
         <p>Conducted a data preprocessing project in Jupyter Notebook analyzing the Wine Quality Data Set from the UCI Machine Learning Repository. Conducted supervised learning, which included data analysis, feature engineering, creating missing values, and prediction of those missing values.</p>
       </li>
       <li>
-        <strong>EncryptionDecryption (Winter 2022)</strong><br />
+      <a href='https://github.com/hamzahap/EncryptionDecryption' target="_blank" rel="noopener noreferrer"><strong>EncryptionDecryption (Winter 2022)</strong></a><br />
         <p>Created a simple Encryption/Decryption/Backup script using PyMySQL, pandas, and Cryptography to back up database tables with CSV files.</p>
       </li>
       <li>
@@ -98,19 +99,19 @@ function App() {
         <p>Built a board game using Java’s Swing and AWT components as a group project. The game allowed for individual player turns, movement of objects, calculating score, and a pathfinding algorithm.</p>
       </li>
       <li>
-        <strong>Hangman (Fall 2017)</strong><br />
+      <a href='https://github.com/hamzahap/HangmanGame' target="_blank" rel="noopener noreferrer"><strong>Hangman (Fall 2017)</strong></a><br />
         <p>Created a hangman game using Python and the tkinter library as a course project. The game utilized a user-created word list to randomly choose words.</p>
       </li>
       <li>
-        <strong>This Website (Winter 2023)</strong><br />
+      <a href='https://github.com/hamzahap/hapunjabi' target="_blank" rel="noopener noreferrer"><strong>This Website (Winter 2023)</strong></a><br />
         <p>Developed a personal website using ReactJS and NodeJS and is hosted on Github Pages. The website showcases my skills, and includes a portfolio of my projects and achievements. Also includes a simple rock shooter game that doesn't make sense but exists.</p>
       </li>
       <li>
-        <strong>My Resume (Winter 2023)</strong><br />
+      <a href='https://github.com/hamzahap/Resume' target="_blank" rel="noopener noreferrer"><strong>My Resume (Winter 2023)</strong></a><br />
         <p>Wrote a professional resume in LaTeX, collaborated with peers to review and revise, and stayed up-to-date with industry trends.</p>
       </li>
       <li>
-        <strong>Advent of Code 2022 (Fall 2023)</strong><br />
+      <a href='https://github.com/hamzahap/AdventOfCode2022' target="_blank" rel="noopener noreferrer"><strong>Advent of Code 2022 (Fall 2023)</strong></a><br />
         <p>Completed the challenge in Google Sheets (Frustrating btw), using formulas and macros to automate data processing and analysis. Completed 10 out of 25 days.</p>
       </li>
       <li>
@@ -118,7 +119,7 @@ function App() {
         <p>As part of a course project, our group investigated the use of natural language processing techniques, such as TF-IDF, Word2vec, and BERT, for automated resume screening.</p>
       </li>
       <li>
-        <strong>Advent of Code 2021 (Fall 2022)</strong><br />
+      <a href='https://github.com/hamzahap/AdventOfCode2021' target="_blank" rel="noopener noreferrer"><strong>Advent of Code 2021 (Fall 2022)</strong></a><br />
         <p>Completed the challenge in Python and numpy, solving problems using data structures and algorithms. Ranked 2nd on the MUN Computer Science Society leaderboard.</p>
       </li>
       <li>
@@ -126,11 +127,11 @@ function App() {
         <p>Completed several beginner-level 2D games using Unity in my free time, including a skating game, a text adventure game, and a number wizard game. These projects demonstrate my interest in game development.</p>
       </li>
       <li>
-        <strong>MUNCS Automata Discord bot (Winter 2021)</strong><br />
+      <a href='https://github.com/MUNComputerScienceSociety/Automata' target="_blank" rel="noopener noreferrer"><strong>MUNCS Automata Discord bot (Winter 2021)</strong></a><br />
         <p>Contributed to the development of a Discord bot used by the MUN CS Society, adding several fun commands to the bot.</p>
       </li>
       <li>
-        <strong>hex16toint (Spring 2022)</strong><br />
+      <a href='https://github.com/hamzahap/hex16toint' target="_blank" rel="noopener noreferrer"><strong>hex16toint (Spring 2022)</strong></a><br />
         <p>Developed a simple PHP/Python script to convert 16 bit hexadecimal numbers to signed integers.</p>
       </li>
       <li>
@@ -147,56 +148,65 @@ function App() {
     <li>
      
       <div>
-        <h3>Python</h3> <FontAwesomeIcon icon={faPython} />
+        <h3>Python</h3> 
         <p>Proficient in data analysis, machine learning, automation scripts, and GUI development with tkinter.</p>
+        <FontAwesomeIcon icon={faPython} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Web Development</h3> <FontAwesomeIcon icon={faHtml5} />
+        <h3>Web Development</h3> 
         <p>Skilled in HTML, CSS, PHP, and JavaScript. Developed responsive web applications using frameworks like React.js, Laravel, Flask, and Node.js.</p>
+        <FontAwesomeIcon icon={faHtml5} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Databases</h3> <FontAwesomeIcon icon={faDatabase} />
+        <h3>Databases</h3> 
         <p>Proficient with relational and NoSQL databases like MySQL, MongoDB, and SQLite. Expertise in query optimization, backups, and data processing.</p>
+        <FontAwesomeIcon icon={faDatabase} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Programming Languages</h3> <FontAwesomeIcon icon={faCode} />
+        <h3>Programming Languages</h3> 
         <p>Experienced in Python, C, C#, C++, Java, Fortran, and PHP. Developed various applications, games, and tools across platforms.</p>
+        <FontAwesomeIcon icon={faCode} />
       </div>
     </li>
     <li>
       <div>
-        <h3>AWS Services</h3> <FontAwesomeIcon icon={faAws} />
+        <h3>AWS Services</h3> 
         <p>Hands-on experience with services like EC2, S3, Lambda, RDS, and others for cloud solutions and deployments.</p>
+        <FontAwesomeIcon icon={faAws} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Version Control</h3> <FontAwesomeIcon icon={faGit} />
+        <h3>Version Control</h3> 
         <p>Proficient in managing codebases and collaboration using Git and platforms like GitHub.</p>
+        <FontAwesomeIcon icon={faGit} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Game Development</h3> <FontAwesomeIcon icon={faGamepad} />
+        <h3>Game Development</h3> 
         <p>Developed games using C++ with SFML, Java, and Unity in C#. Experience in 2D game mechanics, NPC interactions, and multiplayer functionalities.</p>
+        <FontAwesomeIcon icon={faGamepad} />
       </div>
     </li>
     <li>
       <div>
-        <h3>Deployment & DevOps</h3> <FontAwesomeIcon icon={faCloud} />
+        <h3>Deployment & DevOps</h3> 
         <p>Familiar with containerization using Docker and deploying web apps on platforms like Heroku.</p>
+        <FontAwesomeIcon icon={faCloud} />
       </div>
     </li>
     <li>
       <div>
-        <h3>IDEs & Tools</h3> <FontAwesomeIcon icon={faDesktop} />
+        <h3>IDEs & Tools</h3> 
         <p>Proficient in tools like Visual Studio, Jupyter Notebook, and LaTeX for development, data processing, and documentation.</p>
+        <FontAwesomeIcon icon={faDesktop} />
       </div>
     </li>
   </ul>
