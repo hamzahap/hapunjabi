92f23bbaef6cbf15b58b9161e552007d8cbeab82
Hamzah Punjabi <hamzahqatar123@gmail.com>
2023-11-01 04:21:03 -0230

    Add AWS Badge

--- src/App.js
+++ src/App.js
@@ -64,10 +64,10 @@ function App() {
         <h2>Certificates</h2>
         <ul>
           {/* Add your certificates details here */}
-          Play this instead 
+          <center><a href="https://www.credly.com/badges/1769a415-88da-4445-9447-f92b9ae73337" target="_blank" rel="noopener noreferrer"><img src="/badge.png" alt="AWS Badge" /></a></center>
         </ul>
         <br />
-      <RockShooterGame />
+      {/*<RockShooterGame />*/}
       </section>
 
       {/* Projects */}
